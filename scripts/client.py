916cebb4a4daa2e7fad3f5e07311a7a9769862f4
okano_shogo <b1546842@planet.kanazawa-it.ac.jp>
2018-04-01 17:31:34 +0900

    通信用のファイルを入れた

---
+++ scripts/client.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import socket
+
+def main():
+    ip = "192.168.11.6"
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect((ip, 50007))
+
+    while(1):
+        data = soc.recv(1024)
+        print ("Server>", data)       # サーバー側の書き込みを表示
+        data = raw_input("Client>") # 入力待機
+        soc.send(data)              # ソケットに入力したデータを送信
+
+        if data == "q":             # qが押されたら終了
+            soc.close()
+            break
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/server.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import socket
+
+def main():
+    ip = "192.168.11.6"
+    #ip = "localhost"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, 50007))    # 指定したホスト(IP)とポートをソケットに設定
+    s.listen(1)                     # 1つの接続要求を待つ
+    soc, addr = s.accept()          # 要求が来るまでブロック
+    print("Conneted by"+str(addr))  #サーバ側の合図
+
+    while (1):
+        data = raw_input("Server>") # 入力待機(サーバー側)
+        soc.send(data)              # ソケットにデータを送信
+        data = soc.recv(1024)       # データを受信（1024バイトまで）
+        print ("Client>",data)        # サーバー側の書き込みを表示
+        if data == "q":             # qが押されたら終了
+            soc.close()
+            break
+
+if __name__ == '__main__':
+    main()
